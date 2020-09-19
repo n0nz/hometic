833521b8607bcfe9ae77987fec486331cc52b988
n0nz <nonthawat.duangchai@gmail.com>
2020-09-19 15:01:59 +0700

    update

--- server.go
+++ server.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
 )
+
+type Pair struct {
+	DeviceID int64 `json:"device_id"`
+	UserID   int64 `json:"user_id"`
+}
 
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
@@ -35,5 +42,22 @@ func Middleware(h http.Handler) http.Handler {
 }
 
 func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("readall error: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("raw request: %s\n", string(b))
+	defer r.Body.Close()
+
+	var rawRequest Pair
+	if err := json.Unmarshal(b, &rawRequest); err != nil {
+		log.Printf("unmarshal error: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("raw request in struct: %#v\n", rawRequest)
+
 	w.Write([]byte(`{"status":"active"}`))
 }
